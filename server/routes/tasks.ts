73868b236fb08a879c2c908e7e1d9ea51925067f
James <jhk1097@gmail.com>
2023-09-10 02:30:04 +1200

    Made api back end route to get info from db add,del

--- server/routes/tasks.ts
+++ server/routes/tasks.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express'
 import * as db from '../db/tasks.ts'
-import { addNewZombie, deleteById } from '../db/zombies.ts'
+
 const router = Router()
 
 router.get('/', async (req, res) => {
   try {
-    const zombies = await db.getAllZombies()
+    const tasks = await db.getAllTasks()
 
-    res.json(zombies)
+    res.json(tasks)
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
@@ -15,17 +15,17 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-  const newZombie = req.body
-  console.log(newZombie)
-  const addZombie = await db.addNewZombie(newZombie)
-  res.json(addZombie[0])
+  const newTask = req.body
+  console.log(newTask)
+  const addTask = await db.addNewTask(newTask)
+  res.json(addTask[0])
 })
 
 router.delete('/:id', async (req, res) => {
   const deleteId = Number(req.params.id)
   console.log(deleteId)
-  const deleteZombie = await db.deleteById(deleteId)
-  res.json(deleteZombie)
+  const deleteTask = await db.deleteById(deleteId)
+  res.json(deleteTask)
 })
 
 export default router
--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import * as Path from 'node:path'
 
+import taskRoutes from './routes/tasks.ts'
+
 const server = express()
 
 server.use(express.json())
+server.use('/api/v1/zombies', taskRoutes)
 
 if (process.env.NODE_ENV === 'production') {
   server.use(express.static(Path.resolve('public')))
